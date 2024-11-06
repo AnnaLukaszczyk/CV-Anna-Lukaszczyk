8c3478f66e21849f2f8f88abbea08a006cfba37c
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-06 19:08:49 +0100

    simplified styling: removed animations and redundant components

--- src/common/Links/styled.js
+++ src/common/Links/styled.js
@@ -1,97 +1,37 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const rotateAnimation = keyframes`
-  0%, 100% {
-    transform: rotate(0deg);
-  }
-  50% {
-    transform: rotate(30deg);
-  }
-`;
-
-const jumpAnimation = keyframes`
-  0%, 100% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-4px);
-  }
-`;
-
-const blinkLinkAnimation = keyframes`
-  0%, 100% {
-    color: black;
-  }
-  50% {
-    color: #397c90bb;
-  }
-`;
-
-export const IconContainer = styled.span`
-	position: relative;
-	display: inline-block;
-	margin-right: 25px;
-`;
-
-export const IconEnvelope = styled(FontAwesomeIcon)`
-	position: absolute;
-	margin-top: 2px;
-	color: azure;
-	filter: drop-shadow(1px 1px 2px rgb(26, 26, 26));
-	opacity: 1;
-	transition: opacity 0.5s ease-in-out;
-`;
-
-export const IconEnvelopeOpen = styled(FontAwesomeIcon)`
-	position: absolute;
-	color: azure;
-	filter: drop-shadow(1px 1px 2px rgb(26, 26, 26));
-	opacity: 0;
-	transition: opacity 0.5s ease-in-out;
-`;
-
 export const ContactLink = styled.a`
+	display: flex;
+	align-items: center;
 	text-decoration: none;
 	font-weight: bold;
 	color: rgba(64, 64, 64, 1);
 `;
 
-export const LinkToMailContact = styled(ContactLink)`
+export const IconContainer = styled.span`
 	display: flex;
-
-	&:hover ${IconEnvelope} {
-		opacity: 0;
-	}
-
-	&:hover ${IconEnvelopeOpen} {
-		opacity: 1;
-	}
+	align-items: center;
+	justify-content: center;
+	margin-right: 12px;
+	height: 30px;
+	width: 30px;
+	border-radius: 50%;
+	background-color: #3b8097;
 `;
 
 export const Icon = styled(FontAwesomeIcon)`
-	margin-right: 10px;
+	font-size: 16px;
 	color: azure;
 	filter: drop-shadow(1px 1px 2px rgb(26, 26, 26));
 `;
 
-export const LinkToPhoneContact = styled(ContactLink)`
-	&:hover ${Icon} {
-		animation: ${rotateAnimation} 1s infinite;
-	}
-`;
-
 export const LinkToAddressContact = styled(ContactLink)`
 	margin-bottom: 10px;
-
-	&:hover ${Icon} {
-		animation: ${jumpAnimation} 1s infinite;
-	}
 `;
 
 export const Link = styled.a`
 	color: black;
 	font-weight: bold;
 	text-decoration: none;
-	animation: ${blinkLinkAnimation} 3s infinite;
 `;

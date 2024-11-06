97a98eb915f1275812ea101d051ed184dc62dc29
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-06 15:45:28 +0100

    breakpoints removed, colors added

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,5 @@
 export const theme = {
-	breakpoints: {
-		tabletLarge: 920,
-		tablet: 767,
-		tabletSmall: 644,
-		mobileLarge: 575,
-		mobile: 479,
-		mobileMedium: 411,
-		mobileSmall: 365,
+	colors: {
+
 	},
 };

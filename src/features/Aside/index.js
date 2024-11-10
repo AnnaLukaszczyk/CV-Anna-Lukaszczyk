7eb1168d1413a79f28e3114e710fa22778cc6851
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-10 16:22:54 +0100

    refactored Aside component, added conditional content rendering based on the isFirstPage flag

--- src/features/Aside/index.js
+++ src/features/Aside/index.js
@@ -1,47 +1,62 @@
 import { Section } from "../../common/Section";
 import { StyledAside } from "./styled";
-import { AddressLink, EmailLink, PhoneNumberLink, LinkedInLink } from "../../common/Links";
+import {
+	AddressLink,
+	EmailLink,
+	PhoneNumberLink,
+	LinkedInLink,
+} from "../../common/Links";
 
-export const Aside = () => (
+export const Aside = ({ isFirstPage }) => (
 	<StyledAside>
-		<Section
-			name="Kontakt"
-			content={[<EmailLink />, <PhoneNumberLink />, <AddressLink />, <LinkedInLink />]}
-			showIcons={false}
-			showTitle={false}
-		/>
-		<Section
-			name="Umiejętności"
-			content={[
-				"znajomość Responsive Web Design (RWD) i zasad projektowania UI",
-				"znajomość: HTML, CSS (Flexbox, Grid), JavaScript, React",
-				"GitHub",
-				"Figma",
-				"Trello",
-				"zdolność analitycznego myślenia",
-				"umiejętność efektywnego zarządzania czasem",
-				"bezproblemowa praca w zespole",
-				"język angielski - B2",
-			]}
-		/>
-		<Section
-			name="Mocne strony"
-			content={[
-				"kreatywność",
-				"dokładność i szczegółowość",
-				"szybkie uczenie się",
-			]}
-		/>
-		<Section
-			name="Jak pracuję"
-			content={["energicznie", "zadaniowo", "skutecznie", "efektywnie"]}
-		/>
-		<Section
-			name="Zainteresowania"
-			content={[
-				"filmy i seriale kryminalne, detektywistyczne i prawnicze, które angażują w rozwiązywanie spraw i analizowanie złożonych sytuacji",
-				"lubię układać puzzle, które doskonale ćwiczą pamięć i rozwijają umiejętności logicznego myślenia",
-			]}
-		/>
+		{isFirstPage ? (
+			<>
+				<Section
+					name="Kontakt"
+					content={[
+						<EmailLink />,
+						<PhoneNumberLink />,
+						<AddressLink />,
+						<LinkedInLink />,
+					]}
+					showIcons={false}
+					showTitle={false}
+				/>
+				<Section
+					name="Umiejętności"
+					content={[
+						"znajomość Responsive Web Design (RWD) i zasad projektowania UI",
+						"znajomość: HTML, CSS (Flexbox, Grid), JavaScript, React",
+						"GitHub",
+						"Figma",
+						"Trello",
+						"zdolność analitycznego myślenia",
+						"umiejętność efektywnego zarządzania czasem",
+						"bezproblemowa praca w zespole",
+						"język angielski - B2",
+					]}
+				/>
+				<Section
+					name="Mocne strony"
+					content={[
+						"kreatywność",
+						"dokładność i szczegółowość",
+						"szybkie uczenie się",
+					]}
+				/>
+				<Section
+					name="Jak pracuję"
+					content={["energicznie", "zadaniowo", "skutecznie", "efektywnie"]}
+				/>
+			</>
+		) : (
+			<Section
+				name="Zainteresowania"
+				content={[
+					"filmy i seriale kryminalne, detektywistyczne i prawnicze, które angażują w rozwiązywanie spraw i analizowanie złożonych sytuacji",
+					"lubię układać puzzle, które doskonale ćwiczą pamięć i rozwijają umiejętności logicznego myślenia",
+				]}
+			/>
+		)}
 	</StyledAside>
 );

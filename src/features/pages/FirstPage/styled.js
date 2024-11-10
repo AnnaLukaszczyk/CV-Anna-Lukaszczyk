cf8acb57c17a0235fca0e7e56af28f45791125a6
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-10 15:28:43 +0100

    added stylized Course and Description component

---
+++ src/features/pages/FirstPage/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Description = styled.p`
+	text-align: justify;
+`;
+
+export const Course = styled.span`
+	margin-bottom: 4px;
+	font-weight: bold;
+`;

d32f2a753966f0b25731e4c01f2a323f76d09b4a
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-13 14:19:55 +0100

    changed clip-path values ​​for Layer1 and Layer2

--- src/features/Footer/styled.js
+++ src/features/Footer/styled.js
@@ -28,13 +28,13 @@ export const Layer = styled.div`
 
 export const Layer1 = styled(Layer)`
 	background-color: #2e7084;
-	clip-path: polygon(0 100%, 42% 25%, 100% 70%, 100% 100%);
+	clip-path: polygon(0 100%, 50% 25%, 100% 70%, 100% 100%);
 `;
 
 export const Layer2 = styled(Layer)`
 	width: 101%;
 	background-color: #2e7186;
-	clip-path: polygon(0 38%, 90% 100%, 0 100%);
+	clip-path: polygon(0 10%, 90% 100%, 0 100%);
 `;
 
 export const Layer3 = styled(Layer)`

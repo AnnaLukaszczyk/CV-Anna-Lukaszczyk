338519af56b271955d81ce500893e4a9d5f21ca3
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-13 14:13:12 +0100

    updated Aside component: added new skills, strengths and foreign languages

--- src/features/Aside/index.js
+++ src/features/Aside/index.js
@@ -26,37 +26,44 @@ export const Aside = ({ isFirstPage }) => (
 					name="Umiejętności"
 					content={[
 						"znajomość Responsive Web Design (RWD) i zasad projektowania UI",
-						"znajomość: HTML, CSS (Flexbox, Grid), JavaScript, React",
-						"GitHub",
+						"HTML, CSS (Flexbox, Grid)",
+						"JavaScript",
+						"React, Redux",
+						"Bootstrap",
+						"Git, GitHub",
 						"Figma",
 						"Trello",
-						"zdolność analitycznego myślenia",
-						"umiejętność efektywnego zarządzania czasem",
-						"bezproblemowa praca w zespole",
-						"język angielski - B2",
 					]}
 				/>
 				<Section
 					name="Mocne strony"
 					content={[
+						"umiejętność efektywnego zarządzania czasem",
+						"bezproblemowa praca w zespole",
 						"kreatywność",
-						"dokładność i szczegółowość",
-						"szybkie uczenie się",
+						"dokładność i dbałość o szczegóły",
+						"komunikatywność",
+						"zdolność do analitycznego myślenia",
+						"samodyscyplina",
+						"umiejętność szybkiego uczenia się",
 					]}
 				/>
+				<Section name="Języki obce" content={["język angielski - B2"]} />
+			</>
+		) : (
+			<>
 				<Section
 					name="Jak pracuję"
 					content={["energicznie", "zadaniowo", "skutecznie", "efektywnie"]}
 				/>
+				<Section
+					name="Zainteresowania"
+					content={[
+						"filmy i seriale kryminalne, detektywistyczne i prawnicze, które angażują w rozwiązywanie spraw i analizowanie złożonych sytuacji",
+						"lubię układać puzzle, które doskonale ćwiczą pamięć i rozwijają umiejętności logicznego myślenia",
+					]}
+				/>
 			</>
-		) : (
-			<Section
-				name="Zainteresowania"
-				content={[
-					"filmy i seriale kryminalne, detektywistyczne i prawnicze, które angażują w rozwiązywanie spraw i analizowanie złożonych sytuacji",
-					"lubię układać puzzle, które doskonale ćwiczą pamięć i rozwijają umiejętności logicznego myślenia",
-				]}
-			/>
 		)}
 	</StyledAside>
 );

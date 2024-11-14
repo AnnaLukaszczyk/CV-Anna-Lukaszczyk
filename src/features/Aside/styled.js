cfd6a398c62883d813742c80248c3191446434ea
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-14 14:20:41 +0100

    added Info

--- src/features/Aside/styled.js
+++ src/features/Aside/styled.js
@@ -4,8 +4,16 @@ export const StyledAside = styled.aside`
 	padding-top: 210px;
 	padding-bottom: 60px;
 	min-height: 100%;
-	width: 35%;
+	width: 34%;
 	box-shadow: 2px 0 3px gray;
 	overflow: hidden;
 	z-index: 1;
+`;
+
+export const Info = styled.p`
+	margin-top: 180px;
+	padding: 10px 10px 50px;
+	font-size: 11px;
+	color: azure;
+	background-color: #2e7186;
 `;

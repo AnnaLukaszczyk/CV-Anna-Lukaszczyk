a7e3b1933c0c50732ea432df7fc6b590ed10ec82
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-11-14 14:23:54 +0100

    changed width in StyledMain from 65% to 66%

--- src/features/Main/styled.js
+++ src/features/Main/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledMain = styled.main`
 	padding-top: 200px;
 	min-height: 100%;
-	width: 65%;
+	width: 66%;
 	background-color: #e1e1e1;
 	overflow: hidden;
 `;
